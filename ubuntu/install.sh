86f0688f86e85c252f06ac7f67f07feb67e4125a
Chris Ostrum <costrum@costrum2.novalocal>
2015-06-25 02:45:16 +0000

    added ubuntu install

---
+++ ubuntu/install.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+NAME=""
+EMAIL=""
+
+# Install programs
+echo 'Installing programs...'
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install cmake build-essential exuberant-ctags silversearcher-ag git -y
+echo 'Programs installed.'
+
+# Configure them
+echo 'Configuring git...'
+git config --global --replace-all user.name $NAME
+git config --global --replace-all user.email $EMAIL
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
+echo 'Completed git.'
+
+mkdir $HOME/dev
+cd $HOME/dev
+
+git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
+gnome-terminal-colors-solarized/install.sh
+
+git clone https://github.com/seebi/dircolors-solarized.git
