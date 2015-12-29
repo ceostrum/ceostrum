074810e761f398f10225d06ab4d54d0d40d4b9c0
Chris <chris.ostrum@gmail.com>
2015-12-29 10:57:23 -0800

    update ubuntu

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,27 +1,12 @@
 #! /bin/sh
-NAME=""
-EMAIL=""
+NAME="Chris Ostrum"
+EMAIL="chris.ostrum@gmail.com"
 
 mkdir -p $HOME/dev 2> /dev/null
 
-# Install programs
-echo 'Installing programs...'
+echo 'Installing prerequisites...'
+sudo apt-get install automake libevent-dev libncurses5-dev tmux
 
-echo 'Installing VIM'
-cd $HOME/dev
-git clone https://github.com/ceostrum/ceostrum.git
-cd ceostrum/common
-./vim_install.sh
-
-echo 'Installing Google Chrome'
-cd /tmp
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get -f install
-
-echo 'Programs installed.'
-
-# Configure programs
 echo 'Configuring git...'
 git config --global --replace-all user.name $NAME
 git config --global --replace-all user.email $EMAIL
@@ -30,17 +15,30 @@ git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=3600'
 echo 'Completed git.'
 
-echo 'Configuring terminal'
-if [ ! -d "$HOME/dev/gnome-terminal-colors-solarized" ]; then
-  echo 'Setting up Gnome terminal solarized'
-  cd $HOME/dev
+if [ ! -d "$HOME/.solarized" ]; then
+  echo 'Setting up solarized'
+  mkdir -p $HOME/.solarized
+  cd $HOME/.solarized
+  git clone https://github.com/seebi/dircolors-solarized.git
+  eval `dircolors ~/.solarized/dircolors-solarized/dircolors.ansi-dark`
+  ln -s ~/.solarized/dircolors-solarized/dircolors.256dark ~/.dir_colors
+
   git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
-  gnome-terminal-colors-solarized/install.sh -s dark_alternative
+  sh gnome-terminal-colors-solarized/install.sh -s dark_alternative
 fi
 
-if [ ! -d "$HOME/dev/dircolors-solarized" ]; then
-  echo 'Setting up dir colors'
-  cd $HOME/dev
-  git clone https://github.com/seebi/dircolors-solarized.git
-  cp $HOME/dev/dircolors-solarized/dircolors.ansi-dark $HOME/.dir_colors
-fi
+echo 'Setting up Dotfiles'
+cd $HOME/dev
+git clone https://github.com/ceostrum/dotfiles.git
+cp dotfiles/.* $HOME
+
+echo 'Installing VIM'
+cd $HOME/dev/ceostrum/common
+sh vim_install.sh
+
+echo 'Installing Google Chrome'
+cd /tmp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get -f install -y
+
